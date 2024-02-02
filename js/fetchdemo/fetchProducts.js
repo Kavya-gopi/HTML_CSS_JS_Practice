067c1a1e16d11397483e7f3aff6e4939ba6a1748
Kavya-gopi <kavijanu1121@gmail.com>
2024-02-02 13:35:21 +0530

    displayinHTML

--- js/fetchdemo/fetchProducts.js
+++ js/fetchdemo/fetchProducts.js
@@ -11,3 +11,28 @@ const fetchProducts=async ()=>{
 }
 let products=fetchProducts();
 console.log(products);
+
+async function displayProducts(){
+    let content=document.getElementById('content');
+    let products=[];
+    try{
+        products=await fetchProducts();
+    }catch(error){
+        console.log(error);
+    }
+    let newProducts=products.map((p)=>{
+        
+        const {title,description,price,category,image}=p;
+        return `<section>
+        <h4>${title}</h4>
+        <img src="${image}" alt="img" height="140px" width="140px">
+        <h5>${description}</h5>
+        <h6>${category}</h6>
+        <hr>
+        
+        </section>`
+    }).join(' ')
+    content.innerHTML=newProducts;
+
+}
+displayProducts();
